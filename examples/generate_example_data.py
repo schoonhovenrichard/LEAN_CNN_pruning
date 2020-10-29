88f260a644c80bab3c6f2fb907f76caa96bf171e
schoonhovenrichard <r.a.schoonhoven@hotmail.com>
2020-10-29 16:21:58 +0100

    adding examples

---
+++ examples/generate_example_data.py
@@ -0,0 +1,89 @@
+#-----------------------------------------------------------------------
+# NOTE: This file was adapted from http://dmpelt.github.io/msdnet/
+#       to generate quick training data.
+#
+#Copyright 2019 Centrum Wiskunde & Informatica, Amsterdam
+#
+#Author: Daniel M. Pelt
+#Contact: D.M.Pelt@cwi.nl
+#Website: http://dmpelt.github.io/msdnet/
+#License: MIT
+#
+#This file is part of MSDNet, a Python implementation of the
+#Mixed-Scale Dense Convolutional Neural Network.
+#-----------------------------------------------------------------------
+
+import imageio
+import numpy as np
+import tifffile
+from pathlib import Path
+
+n = 256
+nit = 24
+np.random.seed(12345)
+sz = n//8
+
+def generate():
+    im = np.zeros((n,n),dtype=np.float32)
+    l = np.zeros((n,n),dtype=np.uint8)
+
+    tmpl = np.zeros((4,sz,sz),dtype=np.float32)
+    tmpl[0] = 1
+    tmpl[1] = 1
+    tmpl[1][sz//4:-sz//4,sz//4:-sz//4]=0
+    xx,yy = np.mgrid[-1:1:1j*sz,-1:1:1j*sz]
+    tmpl[2] = xx**2+yy**2<1
+    tmpl[3] = xx**2+yy**2<1
+    tmpl[3][xx**2+yy**2<0.25]=0
+
+    i = 0
+    tp = 0
+    while i<nit:
+        found=False
+        while found==False:
+            x, y = (np.random.random(2)*(n-sz)).astype(np.int)
+            if l[x:x+sz,y:y+sz].max()==0:
+                found=True
+        vl = np.random.random()*0.8+0.2
+        im[x:x+sz,y:y+sz] = tmpl[tp]*vl
+        l[x:x+sz,y:y+sz] = tmpl[2*(tp//2)]*(tp+1)
+        tp+=1
+        if tp==4:
+            tp=0
+        i+=1
+
+    imn = im+np.random.normal(size=im.shape)
+    return imn, im, l
+
+pth = Path('data/train')
+pth.mkdir(exist_ok=True)
+(pth / 'noisy').mkdir(exist_ok=True)
+(pth / 'noiseless').mkdir(exist_ok=True)
+(pth / 'label').mkdir(exist_ok=True)
+for i in range(500):
+    imn, im, l = generate()
+    imageio.imsave(pth / 'noisy' / '{:05d}.tiff'.format(i), imn.astype(np.float32))
+    imageio.imsave(pth / 'noiseless' / '{:05d}.tiff'.format(i), im.astype(np.float32))
+    imageio.imsave(pth / 'label' / '{:05d}.tiff'.format(i), l.astype(np.uint8))
+    
+pth = Path('data/val')
+pth.mkdir(exist_ok=True)
+(pth / 'noisy').mkdir(exist_ok=True)
+(pth / 'noiseless').mkdir(exist_ok=True)
+(pth / 'label').mkdir(exist_ok=True)
+for i in range(100):
+    imn, im, l = generate()
+    imageio.imsave(pth / 'noisy' / '{:05d}.tiff'.format(i), imn.astype(np.float32))
+    imageio.imsave(pth / 'noiseless' / '{:05d}.tiff'.format(i), im.astype(np.float32))
+    imageio.imsave(pth / 'label' / '{:05d}.tiff'.format(i), l.astype(np.uint8))
+    
+pth = Path('data/test')
+pth.mkdir(exist_ok=True)
+(pth / 'noisy').mkdir(exist_ok=True)
+(pth / 'noiseless').mkdir(exist_ok=True)
+(pth / 'label').mkdir(exist_ok=True)
+for i in range(50):
+    imn, im, l = generate()
+    imageio.imsave(pth / 'noisy' / '{:05d}.tiff'.format(i), imn.astype(np.float32))
+    imageio.imsave(pth / 'noiseless' / '{:05d}.tiff'.format(i), im.astype(np.float32))
+    imageio.imsave(pth / 'label' / '{:05d}.tiff'.format(i), l.astype(np.uint8))   
---
+++ examples/train_example_msd.py
@@ -0,0 +1,98 @@
+"""
+Adapted from:
+    https://github.com/ahendriksen/msd_pytorch
+"""
+import torch
+import msd_pytorch as mp
+from torch.utils.data import DataLoader
+import numpy as np
+import pathlib
+
+def get_global_accuracy(model, test_dl):
+    # Test on testset
+    device = 'cuda' if torch.cuda.is_available() else 'cpu'
+    correct = 0
+    total = 0
+    model.net.eval()
+    model.msd.eval()
+    for i, (input_im, labels) in enumerate(test_dl):
+        input_im, labels = input_im.to(device), labels.to(device)
+        outputs = model.net(input_im)
+        predicted = torch.argmax(outputs.data, 1)
+        total += labels.numel()
+        correct += (predicted == labels).sum().item()
+    print('     Accuracy of the network on the 10000 test images: {0:.2f}%'.format(100 * correct / float(total)))
+
+if __name__ == '__main__':
+    # The number of input channels of the MSD network
+    c_in = 1
+    # The depth of the MSD network. Good values range between 30 and 200.
+    depth = 50
+    # The width of the MSD network. A value of 1 is recommended.
+    width = 1
+    # The dilation scheme to use for the MSD network. The default is [1,
+    # 2, ..., 10], but [1, 2, 4, 8] is good too.
+    dilations = [1,2,3,4,5,6,7,8,9,10]
+
+    path = str(pathlib.Path().absolute()) + '/data/'
+    train_input_glob = path+"train/noisy/*.tiff"
+    train_target_glob = path+"train/label/*.tiff"
+    val_input_glob = path+"val/noisy/*.tiff"
+    val_target_glob = path+"val/label/*.tiff"
+
+    # The labels to be assigned to the objects
+    labels = [0, 1, 2, 3, 4]
+    # The number of epochs to train for
+    epochs = 50
+    # The mini-batch size used in training.
+    batch_size = 5
+
+    print("Load training dataset")
+    train_ds = mp.ImageDataset(train_input_glob, train_target_glob, labels=labels)
+    train_dl = DataLoader(train_ds, batch_size, shuffle=True)
+
+    # Load Validation dataset (if specified)
+    print("Load validation set")
+    val_ds = mp.ImageDataset(val_input_glob, val_target_glob, labels=labels)
+    val_dl = DataLoader(val_ds, batch_size, shuffle=False)
+
+    print("Create segmentation network model")
+    model = mp.MSDSegmentationModel(
+            c_in, train_ds.num_labels, depth, width, dilations=dilations)
+
+    print("Start estimating normalization parameters")
+    model.set_normalization(train_dl)
+    print("Done estimating normalization parameters")
+
+    print("Starting training...")
+    best_validation_error = np.inf
+    validation_error = 0.0
+
+    train_new_net = False
+    if train_new_net:
+        for epoch in range(epochs):
+            # Train
+            model.train(train_dl, 1)
+            # Compute training error
+            train_error = model.validate(train_dl)
+            print(f"{epoch:05} Training error: {train_error: 0.6f}")
+            # Compute validation error
+            if val_dl is not None:
+                validation_error = model.validate(val_dl)
+                print(f"{epoch:05} Validation error: {validation_error: 0.6f}")
+            # Save network if worthwile
+            if validation_error < best_validation_error or val_dl is None:
+                best_validation_error = validation_error
+                model.save(f"trained_models/msd_network_d={0}_epoch_{1}.torch".format(depth, epoch), epoch)
+
+    # The parameters can be reloaded again:
+    model.load("trained_models/msd_network_d=50_epoch_30.torch")
+
+    # Test on test set
+    test_input_glob = path+"test/noisy/*.tiff"
+    test_target_glob = path+"test/label/*.tiff"
+
+    test_ds = mp.ImageDataset(test_input_glob, test_target_glob, labels=labels)
+    test_dl = DataLoader(test_ds, batch_size, shuffle=False)
+
+    acc = get_global_accuracy(model, test_dl)
