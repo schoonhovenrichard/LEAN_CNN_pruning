0bc9229ae037dd4d2ecb6e3c3ad09c0167f52d08
schoonhovenrichard <r.a.schoonhoven@hotmail.com>
2020-10-29 15:06:15 +0100

    added docstrings for norm calculation methods

--- torch_pruning_methods.py
+++ torch_pruning_methods.py
@@ -10,13 +10,102 @@ import random
 import scipy.linalg
 import scipy.sparse
 
-#import multiprocessing as mp
-#import threading as thr
+def fourier_operatornorm(kern, n, order):
+    r"""Computes the FFT of the filter matrix to efficiently 
+        determine the SVD.
 
-#TODO: Pruning on decay metric like in old/
+    From the paper: The Singular Values of Convolutional Layers, 
+    by Hanie Sedghi, Vineet Gupta, Philip M. Long
+
+    Args:
+        - kern (torch.Tensor): Convolutional filter matrix.
+        - n (int): dimension of the (mock) input image.
+        - order (string): String describing the operator norm.
+            Either 'max' for spectral, 'nuc' for nuclear, or
+            'fro' for Frobenius.
+
+    Returns: norm of filter (float).
+    """
+    filt = kern.detach().cpu().numpy()
+    fourier_coeff = np.fft.fft2(filt, [n,n], axes=[0, 1])
+    if order == 'max':
+        return np.max(np.abs(fourier_coeff))
+    else:
+        S = np.sort(np.absolute(fourier_coeff.flatten()))[::-1]
+        if order == 'nuc':
+            return np.sum(S)
+        elif order == 'fro':
+            frob = np.sum(S**2)
+            return np.sqrt(frob)
+        else:
+            raise Exception("Not implemented order")
+
+def compute_FourierSVD_norms(t, M, orde):
+    r"""Computes the norm of a convolutional layer as interpreted
+        as a matrix operator.
+
+    Args:
+        t (torch.Tensor): tensor representing the parameter to prune
+        M (int): Dimension of (mock) input image.
+        orde (string, int): Order of the norm. Either a string 'nuc'
+            or 'fro', or an integer.
+
+    Returns:
+        norm (list(float)): List of list of norms for all channels,
+            and all inputs for the tensor.
+    """
+    if t.size()[-1] >= 1  and t.size()[-2] >= 1:
+        tnorms = []
+        for x in range(t.size()[0]):
+            normsx = []
+            for y in range(t[x].size()[0]):
+                kernel = t[x][y]
+                knorm = fourier_operatornorm(kernel, M, orde)
+                normsx.append(knorm)
+            tnorms.append(normsx)
+        tnorms = torch.Tensor(tnorms)
+    else:
+        raise Exception("Asked to compute norm of non-convolutional layer!")
+    return tnorms
+
+def compute_Ln_norms_conv(t, N):
+    r"""Computes the norm of a convolutional filter as interpreted
+        as a kxk vector, as opposed to a matrix operator.
+
+    Args:
+        t (torch.Tensor): tensor representing the parameter to prune
+        N (string, int): Order of the norm. Either a string 'nuc'
+            or 'fro', or an integer.
+
+    Returns:
+        norm (list(float)): List of list of norms for all channels,
+            and all inputs for the tensor.
+    """
+    if t.size()[-1] >= 1 and t.size()[-2] >= 1:
+        tnorms = []
+        for x in range(t.size()[0]):
+            normsx = []
+            for y in range(t[x].size()[0]):
+                kernel = t[x][y]
+                if N == 'nuc':
+                    knorm = torch_nuc_norm(kernel)
+                elif N == 'fro':
+                    knorm = torch_fro_norm(kernel)
+                elif isinstance(N, int):
+                    knorm = torch_norm(kernel, N, None)
+                else:
+                    raise Exception("Order not implemented")
+                normsx.append(knorm)
+            tnorms.append(normsx)
+        tnorms = torch.Tensor(tnorms)
+    else:
+        raise Exception("Asked to compute norm of non-convolutional layer!")
+    return tnorms
 
 def torch_norm(t, n, dim):
-    r"""Compute the L_n-norm across all entries in tensor `t` along all dimension 
+    r"""COPY of PyTorch norm implementation!
+
+    Compute the L_n-norm across all entries in tensor `t` along all dimension 
     except for the one identified by dim.
     Example: if `t` is of shape, say, 3x2x4 and dim=2 (the last dim),
     then norm will have Size [4], and each entry will represent the 
@@ -41,86 +130,36 @@ def torch_norm(t, n, dim):
     return norm
 
 def torch_fro_norm(t):
-    r"""Compute the Frobenius-norm across all entries in tensor `t` along all dimensions.
+    r"""Compute the Frobenius-norm across all entries in
+        tensor `t` along all dimensions.
     """
     dims = list(range(t.dim()))
     norm = torch.norm(t.cpu(), p='fro', dim=dims)
     return norm
 
 def torch_nuc_norm(t):
-    r"""Compute the Frobenius-norm across all entries in tensor `t` along all dimensions.
+    r"""Compute the Frobenius-norm across all entries in 
+        tensor `t` along all dimensions.
     """
     dims = list(range(t.dim()))
     norm = torch.norm(t.cpu(), p='nuc', dim=dims)
     return norm
 
-def build_bigH(kern, n, m, order):
-    """For input images of size nxm, this finds the matrix multiplication variant of
-    the 2D convolution and computes the norm. Order can be 'nuc', 'fro' or 2.
-    """
-    kern = kern.cpu().detach().numpy()
-    k1, k2 = kern.shape
-    filtmat = np.zeros(shape=(n+k1-1,m+k2-1),dtype=np.float32)
-    filtmat[-k1:,:k2] = kern
-
-    toeplitz_mats = []
-    for i in range(filtmat.shape[0]-1, -1, -1):
-        c = filtmat[i,:]
-        r = np.concatenate([[c[0]], np.zeros(m-1)])
-        toeplitz_m = scipy.linalg.toeplitz(c,r)
-        toeplitz_mats.append(toeplitz_m)
-
-    c = range(1, filtmat.shape[0]+1)
-    r = np.concatenate([[c[0]], np.zeros(n-1, dtype=int)])
-    doubly_indices = scipy.linalg.toeplitz(c, r)
-    toeplitz_shape = toeplitz_mats[0].shape
-    h = toeplitz_shape[0]*doubly_indices.shape[0]
-    w = toeplitz_shape[1]*doubly_indices.shape[1]
-    doubly_blocked_shape = [h, w]
-    doubly_blocked = np.zeros(doubly_blocked_shape)
-
-    b_h, b_w = toeplitz_shape
-    for i in range(doubly_indices.shape[0]):
-        for j in range(doubly_indices.shape[1]):
-            start_i = i * b_h
-            start_j = j * b_w
-            end_i = start_i + b_h
-            end_j = start_j + b_w
-            doubly_blocked[start_i: end_i, start_j:end_j] = toeplitz_mats[doubly_indices[i,j]-1]
-    return doubly_blocked
-
-def power_operator_norm(A, num_iter=30):
-    x = torch.randn(A.T.shape)
-    for i in range(num_iter):
-        x = A.T@ A @ x.numpy()
-        x = torch.Tensor(x)
-        x /= torch.norm(x)  # L2 vector-norm
-
-    norm_ATA = (torch.norm(torch.Tensor(A.T @ A @ x.numpy())) / torch.norm(x)).item()
-    return math.sqrt(norm_ATA)
-
-def fourier_operatornorm(kern, n, order):
-    r"""Computes the FFT of the filter matrix to efficiently determine the SVD.
-    From the paper: The Singular Values of Convolutional Layers, 
-    by Hanie Sedghi, Vineet Gupta, Philip M. Long
-    """
-    filt = kern.detach().cpu().numpy()
-    if order == 'max':
-        fourier_coeff = np.fft.fft2(filt, [n,n], axes=[0, 1])
-        return np.max(np.abs(fourier_coeff))
-    else:
-        S = np.sort(np.absolute(np.fft.fft2(filt, [n, n]).flatten()))[::-1]
-        if order == 'nuc':
-            return np.sum(S)
-        elif order == 'fro':
-            frob = np.sum(S**2)
-            return np.sqrt(frob)
-        else:
-            raise Exception("Not implemented order")
-
 def operatornorm_bigH(kern, n, m, order):
-    """For input images of size nxm, this finds the matrix multiplication variant of
-    the 2D convolution and computes the norm. Order can be 'nuc', 'fro' or 2.
+    """For input images of size nxm, this finds the matrix 
+        operator norm of the matrix associated to the the 
+        2D convolution. Order can be 'nuc', 'fro' or 2.
+
+        NOTE: Significantly slower than fourier_operatornorm,
+            but is used for checking validity. Also, this 
+            method assumes zero-padding whereas the Fourier
+            method assumes periodic boundary condiditons.
+    Args:
+        - kern (torch.Tensor): Convolutional filter matrix.
+        - n,m (int): dimension of the (mock) input image.
+        - order (string): String describing the operator norm.
+
+    Returns: norm of filter (float).
     """
     kern = kern.cpu().detach().numpy()
     k1, k2 = kern.shape
@@ -157,22 +196,21 @@ def operatornorm_bigH(kern, n, m, order):
         normbigH = np.linalg.norm(doubly_blocked, order)
     return normbigH
 
-def compute_FourierSVD_norms(t, M, orde):
-    if t.size()[-1] >= 1  and t.size()[-2] >= 1:
-        tnorms = []
-        for x in range(t.size()[0]):
-            normsx = []
-            for y in range(t[x].size()[0]):
-                kernel = t[x][y]
-                knorm = fourier_operatornorm(kernel, M, orde)
-                normsx.append(knorm)
-            tnorms.append(normsx)
-        tnorms = torch.Tensor(tnorms)
-    else:
-        raise Exception("Asked to compute norm of non-convolutional layer!")
-    return tnorms
-
 def compute_BigH_norms(t, M, orde):
+    r"""Computes the norm of a convolutional layer as interpreted
+        as a matrix operator. Uses the slower method that actually
+        builds the matrix. Is used to check the validity.
+
+    Args:
+        t (torch.Tensor): tensor representing the parameter to prune
+        M (int): Dimension of (mock) input image.
+        orde (string, int): Order of the norm. Either a string 'nuc'
+            or 'fro', or an integer.
+
+    Returns:
+        norm (list(float)): List of list of norms for all channels,
+            and all inputs for the tensor.
+    """
     if t.size()[-1] == 3 and t.size()[-2]==3:
         tnorms = []
         for x in range(t.size()[0]):
@@ -187,204 +225,43 @@ def compute_BigH_norms(t, M, orde):
         raise Exception("Asked to compute norm of non-convolutional layer!")
     return tnorms
 
-def compute_Ln_norms_conv(t, N):
-    if t.size()[-1] >= 1 and t.size()[-2] >= 1:
-        tnorms = []
-        for x in range(t.size()[0]):
-            normsx = []
-            for y in range(t[x].size()[0]):
-                kernel = t[x][y]
-                if N == 'nuc':
-                    knorm = torch_nuc_norm(kernel)
-                elif N == 'fro':
-                    knorm = torch_fro_norm(kernel)
-                elif isinstance(N, int):
-                    knorm = torch_norm(kernel, N, None)
-                else:
-                    raise Exception("Order not implemented")
-                normsx.append(knorm)
-            tnorms.append(normsx)
-        tnorms = torch.Tensor(tnorms)
-    else:
-        raise Exception("Asked to compute norm of non-convolutional layer!")
-    return tnorms
 
-class CustomLnMethod(prune.BasePruningMethod):
+######################################################################
+### Utility functions that are not used, but useful for inspection ###
+######################################################################
+
+def build_bigH(kern, n, m, order):
+    r"""For input images of size nxm, this finds the matrix 
+        associated to the 2D convolution and computes the 
+        norm. Order can be 'nuc', 'fro' or 2.
     """
-    Structured Ln pruning on each 3x3 convolution
-    """
-    PRUNING_TYPE='structured'
+    kern = kern.cpu().detach().numpy()
+    k1, k2 = kern.shape
+    filtmat = np.zeros(shape=(n+k1-1,m+k2-1),dtype=np.float32)
+    filtmat[-k1:,:k2] = kern
 
-    def __init__(self, amount, n, dim):
-        # Check range of validity of pruning amount
-        prune._validate_pruning_amount_init(amount)
-        self.amount = amount
-        self.n = n
-        self.dim = dim
+    toeplitz_mats = []
+    for i in range(filtmat.shape[0]-1, -1, -1):
+        c = filtmat[i,:]
+        r = np.concatenate([[c[0]], np.zeros(m-1)])
+        toeplitz_m = scipy.linalg.toeplitz(c,r)
+        toeplitz_mats.append(toeplitz_m)
 
-    @classmethod
-    def apply(cls, module, name, amount, n, dim):
-        return super(CustomLnMethod, cls).apply(module, name, amount=amount, n=n, dim=dim)
+    c = range(1, filtmat.shape[0]+1)
+    r = np.concatenate([[c[0]], np.zeros(n-1, dtype=int)])
+    doubly_indices = scipy.linalg.toeplitz(c, r)
+    toeplitz_shape = toeplitz_mats[0].shape
+    h = toeplitz_shape[0]*doubly_indices.shape[0]
+    w = toeplitz_shape[1]*doubly_indices.shape[1]
+    doubly_blocked_shape = [h, w]
+    doubly_blocked = np.zeros(doubly_blocked_shape)
 
-    def compute_mask(self, t, default_mask):
-        if t.size()[-1] == 3 and t.size()[-2]==3:
-            tensor_size = t.shape[self.dim]
-            nparams_toprune = int(round(self.amount * tensor_size))
-            nparams_tokeep = tensor_size - nparams_toprune
-            if nparams_toprune == 0:  # k=0 not supported by torch.kthvalue
-                mask = default_mask
-                return mask
-            if self.n == 'nuc' or self.n == 'fro':
-                tensor_size = int(t.numel()/9)
-                nparams_toprune = int(round(self.amount * tensor_size))
-                nparams_tokeep = tensor_size - nparams_toprune
-                all_norms = []
-                tnorms = []
-                for x in range(t.size()[0]):
-                    normsx = []
-                    for y in range(t[x].size()[0]):
-                        kernel = t[x][y]
-                        if self.n == 'nuc':
-                            knorm = torch_nuc_norm(kernel)
-                        elif self.n == 'fro':
-                            knorm = torch_fro_norm(kernel)
-                        else:
-                            raise Exception("Order not implemented")
-                        if knorm > 0:
-                            all_norms.append(knorm)
-                        normsx.append(knorm)
-                    tnorms.append(normsx)
-                threshold = np.percentile(np.array(all_norms), 100*self.amount)
-
-                mask = torch.ones_like(t)
-                for i in range(len(tnorms)):
-                    for j in range(len(tnorms[i])):
-                        if tnorms[i][j] < threshold:
-                            mask[i][j] = torch.zeros_like(t[i][j])
-                mask *= default_mask.to(dtype=mask.dtype)
-            else:
-                norm = torch_norm(t, self.n, self.dim)
-                topk = torch.topk(norm, k=nparams_tokeep, largest=True,)
-                def make_mask(t, dim, indices):
-                    mask = torch.zeros_like(t)
-                    slc = [slice(None)] * len(t.shape)
-                    slc[dim] = indices
-                    mask[slc] = 1
-                    return mask
-                mask = make_mask(t, self.dim, topk.indices)
-                mask *= default_mask.to(dtype=mask.dtype)
-        else:
-            mask = default_mask
-        return mask
-
-def customln_structured(module, name, amount, n, dim=1):
-    CustomLnMethod.apply(module, name, amount, n, dim)
-    return module
-
-class BigHPruningMethod(prune.BasePruningMethod):
-    PRUNING_TYPE='structured'
-
-    def __init__(self, amount, n, norm, dim):
-        # Check range of validity of pruning amount
-        prune._validate_pruning_amount_init(amount)
-        self.amount = amount
-        self.dim = dim
-        self.n = n
-        self.norm = norm
-
-    @classmethod
-    def apply(cls, module, name, amount, n, norm, dim):
-        return super(BigHPruningMethod, cls).apply(module, name, amount=amount, n=n, norm=norm, dim=dim)
-
-    def operatornorm_bigH_thr(self, kernel, y, outQ): #Deprecated
-        knorm = operatornorm_bigH(kernel, self.n, self.n, self.norm)
-        outQ.put([y,knorm])
-
-    def compute_mask(self, t, default_mask):
-        if t.size()[-1] == 3 and t.size()[-2]==3:
-            tensor_size = int(t.numel()/9)
-            nparams_toprune = int(round(self.amount * tensor_size))
-            nparams_tokeep = tensor_size - nparams_toprune
-            if nparams_toprune == 0:  # k=0 not supported by torch.kthvalue
-                mask = default_mask
-                return mask
-            else:
-                all_norms = []
-                tnorms = []
-                for x in range(t.size()[0]):
-                    normsx = []
-                    for y in range(t[x].size()[0]):
-                        kernel = t[x][y]
-                        knorm = operatornorm_bigH(kernel, self.n, self.n, self.norm)
-                        if knorm > 0:
-                            all_norms.append(knorm)
-                        normsx.append(knorm)
-                    tnorms.append(normsx)
-                threshold = np.percentile(np.array(all_norms), 100*self.amount)
-            
-                tnorms = torch.Tensor(tnorms)
-                mask = torch.ones_like(t)
-                for i in range(len(tnorms)):
-                    for j in range(len(tnorms[i])):
-                        if tnorms[i][j] < threshold:
-                            mask[i][j] = torch.zeros_like(t[i][j])
-                mask *= default_mask.to(dtype=mask.dtype)
-        else:
-            mask = default_mask
-        return mask
-
-def BigH_structured(module, name, amount, n, norm, dim=1):
-    BigHPruningMethod.apply(module, name, amount, n, norm, dim)
-    return module
-
-class FourierSVDPruningMethod(prune.BasePruningMethod):
-    PRUNING_TYPE='structured'
-
-    def __init__(self, amount, n, norm, dim):
-        # Check range of validity of pruning amount
-        prune._validate_pruning_amount_init(amount)
-        self.amount = amount
-        self.dim = dim
-        self.n = n
-        self.norm = norm
-
-    @classmethod
-    def apply(cls, module, name, amount, n, norm, dim):
-        return super(FourierSVDPruningMethod, cls).apply(module, name, amount=amount, n=n, norm=norm, dim=dim)
-
-    def compute_mask(self, t, default_mask):
-        if t.size()[-1] == 3 and t.size()[-2]==3:
-            tensor_size = int(t.numel()/9)
-            nparams_toprune = int(round(self.amount * tensor_size))
-            nparams_tokeep = tensor_size - nparams_toprune
-            if nparams_toprune == 0:  # k=0 not supported by torch.kthvalue
-                mask = default_mask
-                return mask
-            else:
-                all_norms = []
-                tnorms = []
-                for x in range(t.size()[0]):
-                    normsx = []
-                    for y in range(t[x].size()[0]):
-                        kernel = t[x][y]
-                        knorm = fourier_operatornorm(kernel, self.n, self.norm)
-                        if knorm > 0:
-                            all_norms.append(knorm)
-                        normsx.append(knorm)
-                    tnorms.append(normsx)
-                threshold = np.percentile(np.array(all_norms), 100*self.amount)
-            
-                tnorms = torch.Tensor(tnorms)
-                mask = torch.ones_like(t)
-                for i in range(len(tnorms)):
-                    for j in range(len(tnorms[i])):
-                        if tnorms[i][j] < threshold:
-                            mask[i][j] = torch.zeros_like(t[i][j])
-                mask *= default_mask.to(dtype=mask.dtype)
-        else:
-            mask = default_mask
-        return mask
-
-def FourierSVD_structured(module, name, amount, n, norm, dim=1):
-    FourierSVDPruningMethod.apply(module, name, amount, n, norm, dim)
-    return module
+    b_h, b_w = toeplitz_shape
+    for i in range(doubly_indices.shape[0]):
+        for j in range(doubly_indices.shape[1]):
+            start_i = i * b_h
+            start_j = j * b_w
+            end_i = start_i + b_h
+            end_j = start_j + b_w
+            doubly_blocked[start_i: end_i, start_j:end_j] = toeplitz_mats[doubly_indices[i,j]-1]
+    return doubly_blocked
